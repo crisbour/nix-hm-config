ccd1f8fa0b4826c957c4119332779b7dcae6680b
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2023-08-07 11:18:01 +0200

    Python add pre-commmit and jupyter removed

--- modules/python-packages.nix
+++ modules/python-packages.nix
@@ -2,11 +2,12 @@
 
 pkgs.python3.withPackages (p: with p; [
   ipython # interactive shell
-  jupyter # interactive notebooks
+  #jupyter # interactive notebooks
   matplotlib # plots
   networkx # graphs
   numpy # numerical computation
   pandas # data analysis
   pylint # static checking
   setuptools # setup.py
+  pre-commit-hooks
 ])
