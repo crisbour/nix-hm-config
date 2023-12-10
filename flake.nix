cc11842d277fbe63d06ad17f54d373fdb97d0d5e
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2023-12-10 20:55:14 +0000

    Bump nixpkgs version

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
               # Migrate them to home.nix
               inherit username;
               inherit homeDirectory;
-              stateVersion = "23.05";
+              stateVersion = "23.11";
             };
           }
         ];
