e4c978705e5fad87aef6515ebc8775386401c6ad
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-01-12 21:29:09 +0000

    nixGL wrapper fix warning

---
+++ gui/gl_wrapper.nix
@@ -0,0 +1,22 @@
+final: prev:
+
+let
+  nixGLWrap = wrapper: pkg:
+    prev.runCommand "${pkg.name}-nixgl-wrapper" { } ''
+      mkdir $out
+      ln -s ${pkg}/* $out
+      rm $out/bin
+      mkdir $out/bin
+      for bin in ${pkg}/bin/*; do
+       wrapped_bin=$out/bin/$(basename $bin)
+       echo "exec ${prev.lib.getExe' wrapper "nixGL"} $bin \$@" > $wrapped_bin
+       chmod +x $wrapped_bin
+      done
+    '';
+in {
+  alacritty      = nixGLWrap prev.nixgl.auto.nixGLDefault prev.alacritty;
+  joplin-desktop = nixGLWrap prev.nixgl.auto.nixGLDefault prev.joplin-desktop;
+  # FIXME:  error: attribute 'fhsWithPackages' missing
+  #vscode       = nixGLWrap prev.nixgl.auto.nixGLDefault prev.vscode;
+  spotify        = nixGLWrap prev.nixgl.auto.nixGLDefault prev.spotify;
+}
