5919e222f676aa10dd78e83a1bf6d46125f94d4c
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-01-12 21:35:47 +0000

    nvim configs localized to the plugins used

--- modules/vim/vim.nix
+++ modules/vim/vim.nix
@@ -8,16 +8,75 @@ in
   viAlias = true;
   vimAlias = true;
   plugins = with pkgs.vimPlugins; [
-    vim-easy-align
+    {
+      plugin = vim-easy-align;
+      config = ''
+        " EasyAlign Keymaps
+        " Start interactive EasyAlign in visual mode (e.g. vipga)
+        xmap ga <Plug>(EasyAlign)
+        " Start interactive EasyAlign for a motion/text object (e.g. gaip)
+        nmap ga <Plug>(EasyAlign)
+
+        " EasyAlign Delimiter
+        if !exists('g:easy_align_delimiters')
+          let g:easy_align_delimiters = {}
+        endif
+        let g:easy_align_delimiters['d'] = {
+        \ 'pattern': ' \ze\S\+\s*[,;=]',
+        \ 'left_margin': 0, 'right_margin': 0
+        \ }
+      '';
+    }
     #coc-nvim
     editorconfig-vim
-    fzf-vim
+    {
+      plugin = fzf-vim;
+      config = ''
+        let mapleader=' '
+        " fzf bindings
+        nnoremap <leader>r :Rg<CR>
+        nnoremap <leader>b :Buffers<CR>
+        nnoremap <leader>e :Files<CR>
+        nnoremap <leader>l :Lines<CR>
+        nnoremap <leader>L :BLines<CR>
+        nnoremap <leader>c :Commits<CR>
+        nnoremap <leader>C :BCommits<CR>
+      '';
+    }
     fzfWrapper
     LanguageClient-neovim
 #    lightline-vim
-    nerdtree
-    supertab
-    vim-better-whitespace
+    {
+      plugin = nerdtree;
+      config = ''
+        set modifiable
+        "CTRL-t to toggle tree view with CTRL-t
+        nmap <silent> <C-t> :NERDTreeToggle<CR>
+        "Set F2 to put the cursor to the nerdtree
+        nmap <silent> <F2> :NERDTreeFind<CR>
+      '';
+    }
+    {
+      plugin = supertab;
+      config = ''
+        "let g:SuperTabDefaultCompletionType = '<c-x><c-o>'
+
+        if has("gui_running")
+          imap <c-space> <c-r>=SuperTabAlternateCompletion("\<lt>c-x>\<lt>c-o>")<cr>
+        else " no gui
+          if has("unix")
+            inoremap <Nul> <c-r>=SuperTabAlternateCompletion("\<lt>c-x>\<lt>c-o>")<cr>
+          endif
+        endif
+      '';
+    }
+    {
+      plugin = vim-better-whitespace;
+      config = ''
+        let g:better_whitespace_enabled=1
+        let g:strip_whitespace_on_save=1
+      '';
+    }
     vim-visual-multi
 #    vim-multiple-cursors
 #    vim-surround
@@ -38,7 +97,24 @@ in
 
     # rust
     coc-rust-analyzer
-    YouCompleteMe
+
+    {
+      plugin = YouCompleteMe;
+      config = ''
+        let g:ycm_autoclose_preview_window_after_completion = 1
+        let g:ycm_auto_trigger = 0
+        let g:ycm_key_invoke_completion = '<Tab>'
+        let g:ycm_language_server =
+        \ [
+        \   {
+        \     'name': 'rust',
+        \     'cmdline': ['rust-analyzer'],
+        \     'filetypes': ['rust'],
+        \     'project_root_files': ['Cargo.toml']
+        \   }
+        \ ]
+      '';
+    }
     vim-toml
 
     # Julia
@@ -48,10 +124,20 @@ in
     fugitive
 
     # Display tabs for buffers
-    vim-airline
+    {
+      plugin = vim-airline;
+      config = ''
+        " Configure vim-ariline tabline
+        let g:airline#extensions#tabline#enabled = 1
+        let g:airline#extensions#tabline#left_sep = ' '
+        let g:airline#extensions#tabline#left_alt_sep = '|'
 
-    # Tabularize
-    tabular
+        "  Buffer/Tabs navigation
+        :map <C-K> :bnext<CR>
+        :map <C-J> :bprev<CR>
+      '';
+    }
+
 
     # Python linter
     coc-pyright
@@ -110,106 +196,25 @@ in
     "   Plug 'kurkale6ka/vim-swap'
     " call plug#end()
 
-    " Set Codal syntax
-    au BufRead,BufNewFile *.codal set filetype=codal
-    au BufRead,BufNewFile *.hcodal set filetype=codal
-    au FileType codal set syntax=codal.ifdef
-
-
     set list
     set listchars=tab:>-
 
-    let g:better_whitespace_enabled=1
-    let g:strip_whitespace_on_save=1
     let mapleader=' '
 
-    let g:SuperTabDefaultCompletionType = '<c-x><c-o>'
-
-    if has("gui_running")
-      imap <c-space> <c-r>=SuperTabAlternateCompletion("\<lt>c-x>\<lt>c-o>")<cr>
-    else " no gui
-      if has("unix")
-        inoremap <Nul> <c-r>=SuperTabAlternateCompletion("\<lt>c-x>\<lt>c-o>")<cr>
-      endif
-    endif
-
     autocmd FileType markdown setlocal conceallevel=0
-    autocmd FileType cue setlocal shiftwidth=2 tabstop=2 expandtab
-    autocmd FileType cue DisableStripWhitespaceOnSave
-
-    " Format elm files after saving
-    autocmd BufWritePost *.elm silent !elm-format --yes <afile>
-
-    " Tabular bindings
-    if exists(":Tabularize")
-      vmap <leader>a= :Tabularize /=<CR>
-      vmap <leader>a; :Tabularize /::<CR>
-      vmap <leader>a- :Tabularize /-><CR>
-      vmap <leader>a# :Tabularize /#<CR>
-    endif
-
-    " fzf bindings
-    nnoremap <leader>r :Rg<CR>
-    nnoremap <leader>b :Buffers<CR>
-    nnoremap <leader>e :Files<CR>
-    nnoremap <leader>l :Lines<CR>
-    nnoremap <leader>L :BLines<CR>
-    nnoremap <leader>c :Commits<CR>
-    nnoremap <leader>C :BCommits<CR>
-
-
-    " Start interactive EasyAlign in visual mode (e.g. vipga)
-    xmap ga <Plug>(EasyAlign)
-
-    " Start interactive EasyAlign for a motion/text object (e.g. gaip)
-    nmap ga <Plug>(EasyAlign)
-
-    "  Buffer/Tabs navigation
-    :map <C-K> :bnext<CR>
-    :map <C-J> :bprev<CR>
-
-    " Configure vim-ariline tabline
-    let g:airline#extensions#tabline#enabled = 1
-    let g:airline#extensions#tabline#left_sep = ' '
-    let g:airline#extensions#tabline#left_alt_sep = '|'
-
-    let g:cue_fmt_on_save = 0
-    let g:ycm_autoclose_preview_window_after_completion = 1
-    let g:ycm_language_server =
-    \ [
-    \   {
-    \     'name': 'rust',
-    \     'cmdline': ['rust-analyzer'],
-    \     'filetypes': ['rust'],
-    \     'project_root_files': ['Cargo.toml']
-    \   }
-    \ ]
 
     " LanguageClient Settings
     let g:LanguageClient_serverCommands = {
     \ 'rust': ['rust-analyzer'],
     \ }
-    nmap <silent> gd <Plug>(lcn-definition)
 
-    "CTRL-t to toggle tree view with CTRL-t
-    nmap <silent> <C-t> :NERDTreeToggle<CR>
-    "Set F2 to put the cursor to the nerdtree
-    nmap <silent> <F2> :NERDTreeFind<CR>
 
-    " EasyAlign Keymaps
-    " Start interactive EasyAlign in visual mode (e.g. vipga)
-    xmap ga <Plug>(EasyAlign)
-    " Start interactive EasyAlign for a motion/text object (e.g. gaip)
-    nmap ga <Plug>(EasyAlign)
-
-    " EasyAlign Delimiter
-    if !exists('g:easy_align_delimiters')
-      let g:easy_align_delimiters = {}
-    endif
-    let g:easy_align_delimiters['d'] = {
-    \ 'pattern': ' \ze\S\+\s*[,;=]',
-    \ 'left_margin': 0, 'right_margin': 0
-    \ }
+    " TODO: I am not writing lsp, are these still useful to me?
+    nnoremap <silent> <leader>ld    <cmd>lua vim.lsp.buf.declaration()<CR>
+    nnoremap <silent> <leader>lh    <cmd>lua vim.lsp.buf.hover()<CR>
+    nnoremap <silent> <leader>ld    <cmd>lua vim.lsp.util.show_line_diagnostics()<CR>
+    nnoremap <silent> <leader>lk    <cmd>lua vim.lsp.buf.signature_help()<CR>
+    nnoremap <silent> <leader>lr    <cmd>lua vim.lsp.buf.references()<CR>
   '';
 }
 
