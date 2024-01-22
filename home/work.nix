a271f2858aa2dc1741b9ce1dd9e1c429f5437a4a
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-01-22 08:27:52 +0000

    Set user-info and fix overlay setup

--- home/work.nix
+++ home/work.nix
@@ -12,7 +12,7 @@
     overlays = [
         # Get around the issue with openssh on RPM distros: https://nixos.wiki/wiki/Nix_Cookbook
         (final: prev: { openssh = prev.openssh_gssapi; } )
-    ] ++ builtins.getAttrsValue outputs.overlays;
+    ];
   };
 
   home.packages = with pkgs; [
@@ -24,10 +24,11 @@
     username = builtins.getEnv "USER";
     homeDirectory = /. + builtins.getEnv "HOME";
     user-info = {
-      fullName = "Cristian Bourceanu";
-      email = "cristian.bourceanu@codasip.com";
-      gpg.enable = true;
+      fullName      = "Cristian Bourceanu";
+      email         = "cristian.bourceanu@codasip.com";
+      gpg.enable    = true;
       gpg.masterKey = "0x152B728E9A90E3ED";
+      gpg.signKey   = "0x75A90598348541DF";
     };
   };
 }
