2148f4569b41d7dc322f460ab1df698e4e5a8ad8
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-07-08 01:29:56 +0300

    Make user mutable to remove the need to provide hashed password => better security

--- hosts/common/users/cristi.nix
+++ hosts/common/users/cristi.nix
@@ -6,13 +6,11 @@
 }: let
   ifTheyExist = groups: builtins.filter (group: builtins.hasAttr group config.users.groups) groups;
 in {
-  users.mutableUsers = false;
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.cristi = {
     isNormalUser = true;
     description = "Cristi Bourceanu";
     #initialPassword = "ChangeMe1";
-    hashedPassword = "$6$yWS.QK38hcrqXli9$bo.Bq8ElKCp/C9.JZmEa3ONjawBPm0GQ1kZKrnwStcT0hUISHLfXJU1co85PbqNpMQ0nC3BY.Pb45wEpDTZPA0";
     extraGroups = ifTheyExist [
       "docker"
       "wheel"
