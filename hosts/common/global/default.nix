33804e0394727df47ca8d00845d5591a69fda9ee
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-07-08 01:33:03 +0300

    Fixup GSConnect firewall block

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -151,6 +151,14 @@
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.allowedTCPPortRanges = [
+    # KDE Connect
+    { from = 1714; to = 1764; }
+  ];
+  networking.firewall.allowedUDPPortRanges = [
+    # KDE Connect
+    { from = 1714; to = 1764; }
+  ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
