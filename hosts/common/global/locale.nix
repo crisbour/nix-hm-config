aa25c0f43307225bca3eb1d56a1e2cc75a57dbc4
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-09-21 21:21:58 +0100

    Roll back to UK time

--- hosts/common/global/locale.nix
+++ hosts/common/global/locale.nix
@@ -1,7 +1,7 @@
 {lib, ...}: {
 
   # Set your time zone.
-  time.timeZone = "Europe/Bucharest";
+  time.timeZone = "Europe/London";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_GB.UTF-8";
