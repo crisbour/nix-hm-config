c36de8d02e74e422ada133c76dec8416357c289d
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-09-21 21:24:39 +0100

    Add onedrive mount for Microsoft UoE

---
+++ home/features/nfs/default.nix
@@ -0,0 +1,32 @@
+{ pkgs, config, ... }:
+let
+  mount_directory = "${config.home.homeDirectory}/OneDrive";
+in
+{
+  home.packages = with pkgs; [
+    fuse
+    rclone
+  ];
+  systemd.user.services = {
+    rclone-onedrive= {
+      Unit = {
+        Description = "Automount Microsoft OneDrive folder using rclone";
+        AssertPathIsDirectory = mount_directory;
+        Wants = "network-online.target";
+        After = "network-online.target";
+      };
+
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.rclone}/bin/rclone mount --vfs-cache-mode full onedrive: ${mount_directory}";
+        ExecStop = "${pkgs.fuse}/bin/fusermount -zu ${mount_directory}";
+        Restart = "on-failure";
+        RestartSec = 30;
+      };
+
+      Install = {
+        WantedBy = [ "default.target" ];
+      };
+    };
+  };
+}
