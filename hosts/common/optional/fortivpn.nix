600f125b5906a2cafe3a8bbaa235999d3f062afb
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-10-17 10:07:37 +0100

    Fortivpn security fixup: remove password

--- hosts/common/optional/fortivpn.nix
+++ hosts/common/optional/fortivpn.nix
@@ -16,10 +16,9 @@
     ];
 
     etc."openfortivpn/config".text = ''
-      host = "remote.net.ed.ac.uk";
-      port = 8443;
-      username = "s2703496";
-      password = "dHf9|)"R%01S>^3Q";
+      host=remote.net.ed.ac.uk
+      port=8443
+      username=s2703496
     '';
   };
 }
