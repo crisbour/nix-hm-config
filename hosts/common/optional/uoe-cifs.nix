8d477351e64b465972b34ce1e9ff70960a90f51b
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-10-23 12:49:03 +0100

    Allow sshfs and setup correct user mount options

--- hosts/common/optional/uoe-cifs.nix
+++ hosts/common/optional/uoe-cifs.nix
@@ -11,9 +11,15 @@
     fsType = "cifs";
     options = let
       automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,domain=ED,user=s2703496,vers=2.0,noserverino"];
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,domain=ED,user=s2703496,vers=2.0,noserverino,uid=1000,gid=100"];
 
     # Create /etc/nixos/smb-secrets with:
     # password=your_password
+  };
+
+  # ---------- BINDFS ------------
+  # Allow other used to bind mount
+  programs.fuse = {
+    userAllowOther = true;
   };
 }
