15d0024b36a4418f08c1b83a72d5e6b71617d824
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-10-28 10:34:57 +0000

    Add taskwarrior and database location to OneDrive

---
+++ home/features/productivity/taskwarrior/default.nix
@@ -0,0 +1,38 @@
+{
+  config,
+  pkgs,
+  libs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    taskwarrior3
+    taskwarrior-tui
+    #timewarrior # Not sure how to use it
+    tasksh
+    google-cloud-sdk
+  ];
+
+  # Inspired: https://github.com/jevy/home-manager-nix-config/blob/5a088cb8f00e046c9473e39c4c390d31911f495b/taskwarrior/taskrc
+  home.shellAliases = {
+    # Taskwarrior
+    tr = "clear && task ready";
+    t = "clear && task";
+    tt = "taskwarrior-tui";
+    tw = "task waiting";
+    tin = "task add +in";
+  };
+
+  # TODO Add bugwarrior for GitHub, GitLab integration
+
+  # TODO Is it possible to integrate it with Joplin?
+
+  # TODO Implement GTD setup with Inbox: https://github.com/jpcenteno/nixos-config/blob/b9b52ad4974461d6dd353b5a4730c06a83fc0f3d/modules/home-manager/taskwarrior.nix#L9
+
+  # TODO Add gcloud sync: https://man.archlinux.org/man/extra/task/task-sync.5.en
+
+  # TODO Oraganize contexts: https://github.com/roosemberth/dotfiles/blob/1047ab1c9813d91a54a838eb387bba5934901f05/nix/modules/agenda.nix#L20
+
+  home.file = {
+    ".taskrc".source = ./taskrc;
+  };
+}
