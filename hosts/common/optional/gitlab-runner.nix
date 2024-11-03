47cd79b13c5c5a0f5ae666fa041b48e4f8e123b2
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-11-03 12:02:04 +0000

    Configure gitlab-runner service, WIP hide secret

--- hosts/common/optional/gitlab-runner.nix
+++ hosts/common/optional/gitlab-runner.nix
@@ -2,8 +2,16 @@
 {
   boot.kernel.sysctl."net.ipv4.ip_forward" = true; # 1
   virtualisation.docker.enable = true;
+  # TODO: Decide if this is necessary at any point
+  #virtualisation.containers.enable = true;
+
+  # TODO: Add all runners that I am interested about
   services.gitlab-runner = {
     enable = true;
+    settings = {
+      concurrent = 4;
+      #checkInterval = 30;
+    };
     services= {
       # runner for building in docker via host's nix-daemon
       # nix store will be readable in runner, might be insecure
@@ -14,12 +22,17 @@
         # TODO How to use secrets for tokens and ssh keys? https://git.eisfunke.com/config/nixos/-/blob/main/nixos/server/gitlab-runner.nix
         authenticationTokenConfigFile = toString ./ci-env;
         dockerImage = "alpine";
+        # Inspired from https://github.com/pcboy/nix-glab-runner/blob/master/configuration.nix
         dockerVolumes = [
+          "/certs/client"
+          "/cache"
           "/nix/store:/nix/store:ro"
           "/nix/var/nix/db:/nix/var/nix/db:ro"
           "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
+          "/var/run/docker.sock:/var/run/docker.sock"
         ];
-        dockerDisableCache = true;
+        dockerPrivileged = true;
+        dockerDisableCache = false;
         preBuildScript = pkgs.writeScript "setup-container" ''
           mkdir -p -m 0755 /nix/var/log/nix/drvs
           mkdir -p -m 0755 /nix/var/nix/gcroots
@@ -31,7 +44,7 @@
           mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
           mkdir -p -m 0700 "$HOME/.nix-defexpr"
           . ${pkgs.nix}/etc/profile.d/nix-daemon.sh
-          ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-20.09 nixpkgs # 3
+          ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-24.05 nixpkgs
           ${pkgs.nix}/bin/nix-channel --update nixpkgs
           ${pkgs.nix}/bin/nix-env -i ${concatStringsSep " " (with pkgs; [ nix cacert git openssh ])}
         '';
@@ -41,6 +54,9 @@
           NIX_REMOTE = "daemon";
           PATH = "/nix/var/nix/profiles/default/bin:/nix/var/nix/profiles/default/sbin:/bin:/sbin:/usr/bin:/usr/sbin";
           NIX_SSL_CERT_FILE = "/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt";
+          DOCKER_DRIVER = "overlay2";
+          DOCKER_TLS_VERIFY = "1";
+          DOCKER_CERT_PATH = "/certs/client";
         };
       };
     };
