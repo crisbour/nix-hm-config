6b39d906cb6c3b56441220d993f64e2b2ebc3fc1
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-11-03 14:00:09 +0000

    WIP WSL nixos with yubikey passtrough

---
+++ home/wsl.nix
@@ -0,0 +1,27 @@
+{ inputs, outputs, lib, pkgs, ... }:
+{
+  imports = [
+    ./base
+    ./features/auth
+    ./features/devtools
+    ./features/productivity
+    ./features/pass
+    ./features/nfs
+  ];
+
+  home = {
+    # FIXME: Inherit variables
+    username = builtins.getEnv "USER";
+    homeDirectory = /. + builtins.getEnv "HOME";
+    user-info = {
+      username      = "cristi";
+      fullName      = "Cristi Bourceanu";
+      email         = "bourceanu.cristi@gmail.com";
+      github        = "crisbour";
+      gpg.enable    = true;
+      gpg.masterKey = "0xAEF4A543011E8AC1";
+      gpg.signKey   = "0xA6307A244F3BD76D";
+    };
+  };
+
+}
