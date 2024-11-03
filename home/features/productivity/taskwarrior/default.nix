3021b59d08503032807955b8e9cb5cdd02a2d94e
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-11-03 13:54:07 +0000

    Taskwarrior rclone sync

--- home/features/productivity/taskwarrior/default.nix
+++ home/features/productivity/taskwarrior/default.nix
@@ -3,7 +3,14 @@
   pkgs,
   libs,
   ...
-}: {
+}:
+let
+  mount_directory   = "${config.home.homeDirectory}/OneDrive";
+  onedrive_task_dir = "${mount_directory}/Apps/TaskWarrior";
+  local_task_dir    = "${config.home.homeDirectory}/.task";
+in
+
+{
   home.packages = with pkgs; [
     taskwarrior3
     taskwarrior-tui
@@ -28,11 +35,46 @@
 
   # TODO Implement GTD setup with Inbox: https://github.com/jpcenteno/nixos-config/blob/b9b52ad4974461d6dd353b5a4730c06a83fc0f3d/modules/home-manager/taskwarrior.nix#L9
 
-  # TODO Add gcloud sync: https://man.archlinux.org/man/extra/task/task-sync.5.en
-
   # TODO Oraganize contexts: https://github.com/roosemberth/dotfiles/blob/1047ab1c9813d91a54a838eb387bba5934901f05/nix/modules/agenda.nix#L20
 
   home.file = {
     ".taskrc".source = ./taskrc;
   };
+
+  # HACK: Syncronize task DB back and forth, but long term solution is
+  # task-sync: https://github.com/GothenburgBitFactory/taskchampion-sync-server
+  systemd.user.services = {
+    rclone-sync-taskdb = {
+      Unit = {
+        Description = "Bidirectional sync of taskwarrior DB based on timestamps";
+        AssertPathIsDirectory = onedrive_task_dir;
+        Wants = "network-online.target";
+        After = "network-online.target";
+      };
+
+      Service = {
+        Type = "oneshot";
+        ExecStart = ''
+          ${pkgs.rclone}/bin/rclone sync --update ${local_task_dir}/taskchampion.sqlite3 ${onedrive_task_dir}
+          ${pkgs.rclone}/bin/rclone sync --update ${onedrive_task_dir}/taskchampion.sqlite3 ${local_task_dir}
+        '';
+      };
+    };
+  };
+
+  systemd.user.timers = {
+    rclone-sync-taskdb = {
+      Unit = {
+        Description = "Timer for bidirectional taskwarrior DB sync";
+      };
+      Timer = {
+        OnBootSec = "5m";
+        OnUnitActiveSec = "15m";
+      };
+      Install = {
+        WantedBy = [ "timers.target" ];
+      };
+    };
+  };
+
 }
