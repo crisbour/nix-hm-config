1d05c947ffb9bc2d4684346bf2bef28faea87b54
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-12-26 13:21:01 +0200

    Add group datastore cifs

--- hosts/common/optional/uoe-cifs.nix
+++ hosts/common/optional/uoe-cifs.nix
@@ -17,6 +17,17 @@
     # password=your_password
   };
 
+  fileSystems."/mnt/datastore-3D" = {
+    device = "//csce.datastore.ed.ac.uk/csce/eng/groups/3D_VISION";
+    fsType = "cifs";
+    options = let
+      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
+    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,domain=ED,user=s2703496,vers=2.0,noserverino,uid=1000,gid=100"];
+
+    # Create /etc/nixos/smb-secrets with:
+    # password=your_password
+  };
+
   # ---------- BINDFS ------------
   # Allow other used to bind mount
   programs.fuse = {
