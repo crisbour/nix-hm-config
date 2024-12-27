5fff216544755d753239f6fd8cde5a2079af8600
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2024-12-27 05:20:03 +0200

    curate some configs for the server

--- hosts/hydrax/default.nix
+++ hosts/hydrax/default.nix
@@ -16,6 +16,29 @@
     ../common/optional/lxd.nix
   ];
 
+  time.timeZone = "Europe/Bucharest";
+
+  boot.loader.grub = {
+    enable = true;
+    version = 2;
+    forceInstall = true;
+    device = "/dev/nvme0n1p1";
+  };
+
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "yes";
+    passwordAuthentication = false;
+  };
+
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGySvc1K+NCd+b/az6ZhtscwM3XO0hnLnB/CWavpow5T"
+  ];
+
+  users.users.cristi.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGySvc1K+NCd+b/az6ZhtscwM3XO0hnLnB/CWavpow5T"
+  ];
+
   # TODO: Add HM inline to configure user
 
   system.stateVersion = "24.05";
