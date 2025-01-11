ebcabcc01f2a01bde9ba3f2707c60aba3d1f9fd8
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-01-11 17:52:29 +0000

    Remove codasip ssh dest

--- home/features/auth/ssh.nix
+++ home/features/auth/ssh.nix
@@ -3,26 +3,12 @@
     enable = true;
     forwardAgent = true;
     matchBlocks = {
-			slurmlogin1 = {
-  			hostname = "in-czba-psl0001.slurm.codasip.com";
-  			user = "cristian.bourceanu";
-  			extraOptions = {
-					GSSAPIDelegateCredentials = "yes";
-				};
-			};
 
-			slurmlogin2 = {
-  			hostname = "in-czba-psl0002.slurm.codasip.com";
-  			user = "cristian.bourceanu";
-  			extraOptions = {
-					GSSAPIDelegateCredentials = "yes";
-				};
-			};
-
-			vdilogin = {
-  			hostname = "10.13.72.10";
-  			user = "cristian.bourceanu";
-			};
-		};
-	};
+      nexus = {
+        hostname = "adventure-bytes.com";
+        user = "cristi";
+        port = 5022;
+      };
+    };
+  };
 }
