4cf7bfc41bb56b220cd6cac239babd9f61c66bd0
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-01-11 19:06:39 +0000

    Unify configs based on system param

--- hosts/common/optional/docker.nix
+++ hosts/common/optional/docker.nix
@@ -1,30 +1,7 @@
-{ pkgs, ... }:
+{ config, ... }:
 {
-  virtualisation.docker = {
+  mySystem.docker = {
     enable = true;
-    daemon.settings = {
-      #cdi = true;
-      fixed-cidr-v6 = "fd00::/80";
-      ipv6 = true;
-    };
+    nvidia.enable = config.mySystem.info.has_nvidia;
   };
-  # FIXME Only enable if NVIDIA is present. Add a config value in modules
-  # HACK: Conditional assert on nvidia driver installed
-  hardware.nvidia-container-toolkit.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
-
-  users.users.cristi.extraGroups = ["docker"];
-
-  environment.systemPackages = with pkgs; [
-    docker
-    xorg.xhost # Necessary for allowing docker X11 access
-  ];
-
-  # kernel module for forwarding to container to work
-  boot.kernelModules = [ "nf_nat_ftp" ];
-
-   # Export X11 host to docker
-  environment.shellInit = ''
-    [ -n "$DISPLAY" ] && xhost +si:localuser:$USER || true
-  '';
 }
