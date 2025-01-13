8aae416d5168ef860a346406c6970c84d0484b9d
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-01-13 04:40:09 +0000

    Taswarrior sync config wip

--- home/features/productivity/taskwarrior/default.nix
+++ home/features/productivity/taskwarrior/default.nix
@@ -4,12 +4,6 @@
   libs,
   ...
 }:
-let
-  mount_directory   = "${config.home.homeDirectory}/OneDrive";
-  onedrive_task_dir = "${mount_directory}/Apps/TaskWarrior";
-  local_task_dir    = "${config.home.homeDirectory}/.task";
-in
-
 {
   home.packages = with pkgs; [
     taskwarrior3
@@ -17,6 +11,11 @@ in
     #timewarrior # Not sure how to use it
     tasksh
   ];
+
+  services.taskwarrior-sync = {
+    enable = true;
+    frequency = "hourly";
+  };
 
   # Inspired: https://github.com/jevy/home-manager-nix-config/blob/5a088cb8f00e046c9473e39c4c390d31911f495b/taskwarrior/taskrc
   home.shellAliases = {
@@ -40,40 +39,5 @@ in
     ".taskrc".source = ./taskrc;
   };
 
-  # HACK: Syncronize task DB back and forth, but long term solution is
-  # task-sync: https://github.com/GothenburgBitFactory/taskchampion-sync-server
-  systemd.user.services = {
-    rclone-sync-taskdb = {
-      Unit = {
-        Description = "Bidirectional sync of taskwarrior DB based on timestamps";
-        AssertPathIsDirectory = onedrive_task_dir;
-        Wants = "network-online.target";
-        After = "network-online.target";
-      };
-
-      Service = {
-        Type = "oneshot";
-        ExecStart = ''
-          ${pkgs.rclone}/bin/rclone sync --update ${local_task_dir}/taskchampion.sqlite3 ${onedrive_task_dir}
-          ${pkgs.rclone}/bin/rclone sync --update ${onedrive_task_dir}/taskchampion.sqlite3 ${local_task_dir}
-        '';
-      };
-    };
-  };
-
-  systemd.user.timers = {
-    rclone-sync-taskdb = {
-      Unit = {
-        Description = "Timer for bidirectional taskwarrior DB sync";
-      };
-      Timer = {
-        OnBootSec = "5m";
-        OnUnitActiveSec = "15m";
-      };
-      Install = {
-        WantedBy = [ "timers.target" ];
-      };
-    };
-  };
 
 }
