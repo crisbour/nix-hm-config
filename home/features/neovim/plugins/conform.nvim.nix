d0595fdf661c2c453e3dd229ee9fd297ee88d034
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-01-24 20:07:14 +0000

    Add lsp support for verilog and rust

--- home/features/neovim/plugins/conform.nvim.nix
+++ home/features/neovim/plugins/conform.nvim.nix
@@ -8,11 +8,14 @@
           go = { "goimports" },
           markdown = { "deno_fmt" },
           nix = { "nixfmt" },
+          systemverilog = { "verible" },
+          verilog = { "verible" },
         },
         formatters = {
           deno_fmt = { command = "${pkgs.deno}/bin/deno" },
           goimports = { command = "${pkgs.gotools}/bin/goimports" },
           nixfmt = { command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt" },
+          verible = { command = "${pkgs.verible}/bin/verible-verilog-format" },
         },
       })
       vim.keymap.set('n', '<C-f>', function()
