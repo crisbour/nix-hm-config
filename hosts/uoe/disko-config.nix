ee090e1a89d20e93dfd73a133c4c573f1b3bea63
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-01-24 20:13:05 +0000

    Add uoe desktop

---
+++ hosts/uoe/disko-config.nix
@@ -0,0 +1,90 @@
+{
+  disko.devices = {
+    # TODO: If using imperanence
+    #nodev."/" = {
+    #  fsType = "tmpfs";
+    #  mountOptions = [ "defaults" "size=2G" "mode=755" ];
+    #};
+    nodev = {
+      "/tmp" = {
+        fsType = "tmpfs";
+        mountOptions = [
+          "defaults"
+          "size=4G"
+          "mode=755"
+        ];
+      };
+    };
+    disk.main = {
+      type = "disk";
+      device = "/dev/nvme0n1";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            priority = 0;
+            size = "700M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot";
+              mountOptions = [ "umask=0077" ];
+            };
+          };
+          crypt-root = {
+            priority = 100;
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "crypt-root";
+              settings = {
+                allowDiscards = true;
+                bypassWorkqueues = true;
+              };
+              content = {
+                type = "btrfs";
+                subvolumes = let
+                  # NOTE: x-gvfs-hide is useful to hiding the route of such mouting points in GUI
+                  mountOptions = ["compress=zstd" "noatime" ];
+                in {
+                  # TODO: The root can go away if we opt for impermanence
+                  "@root" = {
+                    mountpoint = "/";
+                    inherit mountOptions;
+                  };
+                  "@persist" = {
+                    mountpoint = "/persist";
+                    inherit mountOptions;
+                  };
+                  "@var-log" = {
+                    mountpoint = "/var/log";
+                    inherit mountOptions;
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    inherit mountOptions;
+                  };
+                  # NOTE: Home will be part of persist
+                  "@home" = {
+                    mountpoint = "/home";
+                    inherit mountOptions;
+                  };
+                  "@swap" = {
+                    mountpoint = "/swap";
+                    mountOptions = [ "noatime" ];
+                    swap.swapfile.size = "32G";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
+
+  #fileSystems."/persist".neededForBoot = true;
+  #fileSystems."/var/log".neededForBoot = true;
+}
