38d2389e7f8472999500da4cc9081ea41159a522
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-01-25 17:09:20 +0000

    Add syncthing to w9098 as well

---
+++ hosts/uoe/services/syncthing.nix
@@ -0,0 +1,38 @@
+{
+  services = {
+    syncthing = {
+      enable = true;
+      #group = "mygroupname";
+      user = "cristi";
+      dataDir = "/home/cristi/Documents";    # Default folder for new synced folders
+      configDir = "/home/cristi/Documents/.config/syncthing";   # Folder for Syncthing settings and keys
+
+       # Open necessary ports for Syncthing
+      openDefaultPorts = true;
+
+      # WARN: Secure the web interface
+      #settings.gui = {
+      #  user = "cristi";
+      #  password = "your-secure-password";
+      #};
+
+      # Declarative device and folder configuration
+      settings = {
+        devices = {
+          "precision" = { id = "ZXJRZV4-ZLEGFRF-4DHGLFV-DYIBCFJ-K7JEYDQ-KBHZFKI-M4PYZ65-7LJVIQX"; };
+          "w9098" = { id = "DEVICE-ID-FROM-W9098"; };
+        };
+
+        folders = {
+          "Documents" = {
+            path = "/home/cristi/Documents";
+            devices = [ "precision" "w9098" ];
+          };
+        };
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+}
