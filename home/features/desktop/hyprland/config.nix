5f6b1a642529c937b1ae66c8759bd1b13bb520c0
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-01-25 16:31:09 +0000

    Separate polkit agent

--- home/features/desktop/hyprland/config.nix
+++ home/features/desktop/hyprland/config.nix
@@ -8,9 +8,6 @@
         "dbus-update-activation-environment --all --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
         "systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP"
 
-        # Elevate permissions
-        "systemctl --user start hyprpolkitagent"
-
         # Fix screensharing for X11 applications
         "xwaylandvideobridge &"
 
