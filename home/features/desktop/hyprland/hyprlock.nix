3587821ad2d16d2672097b72e3c809e9a661cdfa
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-01-25 16:29:20 +0000

    Change hyprlock style to solve screen tearing

--- home/features/desktop/hyprland/hyprlock.nix
+++ home/features/desktop/hyprland/hyprlock.nix
@@ -1,121 +1,94 @@
-{ pkgs, ... }:
+# Hyprlock is a lockscreen for Hyprland
+{ config, lib, ... }:
+# Inspired from Nixy: https://github.com/anotherhadi/nixy/blob/main/home/system/hyprpanel/default.nix
 let
-  text = "rgb(251, 241, 199)";
-  host = "laptop";
-  # TODO: Receive this from config
-  globalFont = "Noto Sans Mono";
-in
-{
-  home.packages = [ pkgs.hyprlock ];
-  xdg.configFile."hypr/hyprlock.conf".text = ''
-    # BACKGROUND
-    background {
-      monitor =
-      path = ${../../../../wallpapers/Amanita.jpg}
-      blur_passes = 2
-      contrast = 0.8916
-      brightness = 0.8172
-      vibrancy = 0.1696
-      vibrancy_darkness = 0.0
-    }
+  foreground = "rgba(216, 222, 233, 0.70)";
+  #imageStr = toString config.stylix.image;
+  imageStr = "${../../../../wallpapers/cycling_petite_venice.jpg}";
+  #font = config.stylix.fonts.serif.name;
+  font = "Noto Sans Mono";
+in {
+  programs.hyprlock = {
+    enable = true;
+    settings = {
+      general = {
+        grace = 5;
+        no_fade_in = false;
+        disable_loading_bar = false;
+      };
 
-    # GENERAL
-    general {
-      hide_cursor = true
-      no_fade_in = false
-      grace = 0
-      disable_loading_bar = false
-      ignore_empty_input = true
-      fractional_scaling = 0
-    }
+      # BACKGROUND
+      background = {
+        monitor = "";
+        #path = imageStr;
+        blur_passes = 1;
+        blur_size = 1;
+        noise = 0;
+        contrast = 0.8916;
+        brightness = 0.6;
+        vibrancy = 0.2696;
+        vibrancy_darkness = 0.05;
+      };
 
-    # Authentication
-    auth {
-      fingerprint {
-        enabled = true
-        #ready_message = "Waitting finger scan"
-        #present_message = "Scanning fingerprint"
-      }
-      pam {
-        enabled = true
-      }
-    }
+      label = [
+        {
+          # Day-Month-Date
+          monitor = "";
+          text = ''cmd[update:1000] echo -e "$(date +"%A, %B %d")"'';
+          color = foreground;
+          font_size = 28;
+          font_family = font + " Bold";
+          position = "0, 490";
+          halign = "center";
+          valign = "center";
+        }
+        # Time
+        {
+          monitor = "";
+          text = ''cmd[update:1000] echo "<span>$(date +"%I:%M")</span>"'';
+          color = foreground;
+          font_size = 160;
+          font_family = "steelfish outline regular";
+          position = "0, 370";
+          halign = "center";
+          valign = "center";
+        }
+        # USER
+        {
+          monitor = "";
+          text = "    $USER";
+          color = foreground;
+          outline_thickness = 2;
+          dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
+          dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
+          dots_center = true;
+          font_size = 18;
+          font_family = font + " Bold";
+          position = "0, -180";
+          halign = "center";
+          valign = "center";
+        }
+      ];
 
-    # Time
-    label {
-      monitor =
-      text = cmd[update:1000] echo "$(date +"%k:%M")"
-      color = rgba(235, 219, 178, .9)
-      font_size = 115
-      font_family = ${globalFont}
-      shadow_passes = 3
-      position = 0, ${if (host == "laptop") then "-25" else "-150"}
-      halign = center
-      valign = top
-    }
-
-    # Day
-    label {
-      monitor =
-      text = cmd[update:1000] echo "- $(date +"%A, %B %d") -"
-      color = rgba(235, 219, 178, .9)
-      font_size = 18
-      font_family = ${globalFont}
-      shadow_passes = 3
-      position = 0, ${if (host == "laptop") then "-225" else "-350"}
-      halign = center
-      valign = top
-    }
-
-
-    # USER-BOX
-    shape {
-      monitor =
-      size = 300, 50
-      color = rgba(102, 92, 84, .33)
-      rounding = 10
-      border_size = 0
-      border_color = rgba(255, 255, 255, 0)
-      rotate = 0
-
-      position = 0, ${if (host == "laptop") then "120" else "270"}
-      halign = center
-      valign = bottom
-    }
-
-    # USER
-    label {
-      monitor =
-      text =   $USER
-      color = rgba(235, 219, 178, 1)
-      font_size = 15
-      font_family = ${globalFont}
-      position = 0, ${if (host == "laptop") then "131" else "281"}
-      halign = center
-      valign = bottom
-    }
-
-    # INPUT FIELD
-    input-field {
-      monitor =
-      size = 300, 50
-      outline_thickness = 1
-      rounding = 10
-      dots_size = 0.25 # Scale of input-field height, 0.2 - 0.8
-      dots_spacing = 0.4 # Scale of dots' absolute size, 0.0 - 1.0
-      dots_center = true
-      outer_color = rgba(102, 92, 84, .33)
-      inner_color = rgba(102, 92, 84, .33)
-      color = rgba(235, 219, 178, .9)
-      font_color = rgba(235, 219, 178, .9)
-      font_size = 14
-      font_family = ${globalFont}
-      fade_on_empty = false
-      placeholder_text = <i><span foreground="##fbf1c7">Enter Password</span></i>
-      hide_input = false
-      position = 0, ${if (host == "laptop") then "50" else "200"}
-      halign = center
-      valign = bottom
-    }
-  '';
+      # INPUT FIELD
+      input-field = lib.mkForce {
+        monitor = "";
+        size = "300, 60";
+        outline_thickness = 2;
+        dots_size = 0.2; # Scale of input-field height, 0.2 - 0.8
+        dots_spacing = 0.2; # Scale of dots' absolute size, 0.0 - 1.0
+        dots_center = true;
+        outer_color = "rgba(255, 255, 255, 0)";
+        inner_color = "rgba(255, 255, 255, 0.1)";
+        font_color = foreground;
+        fade_on_empty = false;
+        font_family = font + " Bold";
+        placeholder_text = "<i>🔒 Enter Password</i>";
+        hide_input = false;
+        position = "0, -250";
+        halign = "center";
+        valign = "center";
+      };
+    };
+  };
 }
