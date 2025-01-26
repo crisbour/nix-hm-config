eac1170d2e689c79787e8609173cacf7ae912fff
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-01-26 11:55:46 +0000

    Stylix fixup and clean nix theme config

--- home/features/desktop/hyprland/stylix.nix
+++ home/features/desktop/hyprland/stylix.nix
@@ -3,7 +3,7 @@
 {
   stylix = {
     enable = true;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+    base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
      # Edited catppuccin
     #base16Scheme = {
     #  base00 = "0b0b0b"; # Default Background
@@ -40,7 +40,8 @@
     fonts = {
       monospace = {
         package = (pkgs.nerdfonts.override { fonts = [ "FiraCode" "Iosevka" "NerdFontsSymbolsOnly" "JetBrainsMono" ]; });
-        name = "JetBrains Mono Nerd Font";
+        #name = "JetBrains Mono Nerd Font";
+        name = "Iosevka Nerd Font";
       };
       sansSerif = {
         name = "Noto Sans";
--- home/features/neovim/theme.nix
+++ home/features/neovim/theme.nix
@@ -3,19 +3,9 @@ with lib; {
   programs.neovim = {
     plugins = with pkgs.vimPlugins; [
       nvim-web-devicons
-      #nightfox-nvim
-      gruvbox
     ];
 
     extraConfig = ''
-    if ($TERM == 'alacritty' || $TERM == 'tmux-256color' ||
-      \ $TERM == 'xterm-256color' || $TERM == 'screen-256color') && !has('gui_running')
-          set termguicolors
-      endif
-
-      colorscheme gruvbox
-      set background=dark
-
       " Highlight past 100 characters
       highlight Over100Length ctermbg=red ctermfg=white guibg=#BD4F4F guifg=#cccccc
       match Over100Length /\%101v.\+/
