5a7106a44677b93330cec5f662ce094847fa6a51
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-02-12 19:31:41 +0000

    gitlab-runner use podman, add julia cache and note cachix migration future

--- hosts/common/optional/gitlab-runner.nix
+++ hosts/common/optional/gitlab-runner.nix
@@ -1,9 +1,14 @@
 { pkgs, lib, config, ... }:
 {
   boot.kernel.sysctl."net.ipv4.ip_forward" = true; # 1
-  virtualisation.docker.enable = true;
-  # TODO: Decide if this is necessary at any point
+
+  # TODO: Check if there is a container option already setup, otherwise default to podman or docker
   #virtualisation.containers.enable = true;
+  virtualisation.oci-containers.backend = "podman";
+
+  systemd.services.gitlab-runner = {
+    serviceConfig.SupplementaryGroups = [ "podman" ];
+  };
 
   # TODO: Add all runners that I am interested about
   services.gitlab-runner = {
@@ -22,7 +27,12 @@
         # TODO How to use secrets for tokens and ssh keys? https://git.eisfunke.com/config/nixos/-/blob/main/nixos/server/gitlab-runner.nix
         authenticationTokenConfigFile = config.sops.templates."gitlab-runner-uoe-auth".path;
         dockerImage = "alpine";
+
         # Inspired from https://github.com/pcboy/nix-glab-runner/blob/master/configuration.nix
+
+        # FIXME: Configure server to run a cachix service and grab the binaries
+        # from there instead of expecting local user to have run `nix develop`
+        # or require to polute system /nix/store
         dockerVolumes = [
           "/certs/client"
           "/cache"
@@ -30,7 +40,16 @@
           "/nix/var/nix/db:/nix/var/nix/db:ro"
           "/nix/var/nix/daemon-socket:/nix/var/nix/daemon-socket:ro"
           "/var/run/docker.sock:/var/run/docker.sock"
+          "/var/lib/julia:/root/.julia"
+          #"/var/run/podman/podman.sock:/var/run/podman/podman.sock"
         ];
+
+        # WARN: This is insecure, but I don't know how to use fuse otherwise
+        # I need access to add the following parameters to the service:
+        # --cap-add SYS_ADMIN --device /dev/fuse
+        # -> I could copy the service module from upstream and tweak it
+        # Fixup: Use podman instead of docker, furthermore, could pass
+        # --cap-add SYS_ADMIN and --device /dev/fuse to constrain privileges
         dockerPrivileged = true;
         dockerDisableCache = false;
         preBuildScript = pkgs.writeScript "setup-container" ''
@@ -44,7 +63,7 @@
           mkdir -p -m 0755 /nix/var/nix/profiles/per-user/root
           mkdir -p -m 0700 "$HOME/.nix-defexpr"
           . ${pkgs.nix}/etc/profile.d/nix-daemon.sh
-          ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-24.05 nixpkgs
+          ${pkgs.nix}/bin/nix-channel --add https://nixos.org/channels/nixos-24.11 nixpkgs
           ${pkgs.nix}/bin/nix-channel --update nixpkgs
           ${pkgs.nix}/bin/nix-env -i ${concatStringsSep " " (with pkgs; [ nix cacert git openssh ])}
         '';
