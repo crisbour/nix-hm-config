674f8fd89e2fe40a59679633ef5eb55342e91344
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-02-12 19:38:48 +0000

    WIP config vscode for analysis and experiments usage

--- home/features/devtools/vscode.nix
+++ home/features/devtools/vscode.nix
@@ -1,6 +1,31 @@
 {config, pkgs, ...}:
 let
   hasGUI = config.home.user-info.has_gui;
+  packagedExtensions = with pkgs.vscode-extensions; [
+      asciidoctor.asciidoctor-vscode
+      vscodevim.vim
+    #asvetliakov.vscode-neovim
+      ms-python.python
+      arrterian.nix-env-selector
+    #pinage404.nix-extension-pack
+      bbenoist.nix
+      #pinage404.nix-extension-pack
+      editorconfig.editorconfig
+      rust-lang.rust-analyzer
+      #ms-azuretools.vscode-docker
+      #ms-python.python
+      #ms-python.vscode-pylance
+      ms-toolsai.jupyter
+      #ms-vscode.cpptools
+  ];
+  marketplaceExtensions = pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+    {
+      name = "vscode-sshfs";
+      publisher = "Kelvin";
+      version = "1.25.0";
+      sha256 = "7358e9649e54b23787d7e343f4d71777a14ab2f0acf093051a649d2b6aeea26c";
+    }
+  ];
 in
 {
 
@@ -11,11 +36,34 @@ in
   programs.vscode = {
     enable = hasGUI;
     package = pkgs.vscode-fhsWithPackages (pkgs: with pkgs; [ zlib rustup ]);
-    extensions = with pkgs.vscode-extensions; [
-      asciidoctor.asciidoctor-vscode
-      vscodevim.vim
-      ms-python.python
-      # TODO: Add Codal extension with fetchURL
-    ];
+    extensions = packagedExtensions ++ marketplaceExtensions;
+    userSettings = {
+      #"vim.enableNeovim" = true;
+      #"vim.neovimPath" = "/home/cristi/.nix-profile/bin/nvim";
+
+      # Visual: Enable ligatures
+      "editor.fontLigatures" = true;
+
+      # Third-party extensions
+      "jupyter.askForKernelRestart" = false;
+      "jupyter.diagnostics.reservedPythonNames.enabled" = false;
+      "rust-analyzer.lens.enable" = false;
+      "rust-analyzer.inlayHints.enable" = false;
+      # TODO: Configure vs-code and neovim integration: https://medium.com/@nikmas_dev/vscode-neovim-setup-keyboard-centric-powerful-reliable-clean-and-aesthetic-development-582d34297985
+
+      # Neovim support
+      #"extensions.experimental.affinity" = {
+      #  "asvetliakov.vscode-neovim" = 1;
+      #};
+      #"vscode-neovim.neovimExecutablePaths.linux" = /home/cristi/.nix-profile/bin/nvim;
+      #"vscode-neovim.neovimInitVimPaths.linux" = "$HOME/.config/nvim/init.lua";
+    };
+    #idx.extensions = [
+    #  "mhutchie.git-graph"
+    #  "streetsidesoftware.code-spell-checker"
+    #  "oderwat.indent-rainbow"
+    #];
+    #keybindings = [
+    #];
   };
 }
