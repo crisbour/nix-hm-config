62e506e5f9afd83558588f755219aebe4feeaf4f
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-04-11 13:25:14 +0100

    WIP fixup

--- home/wsl.nix
+++ home/wsl.nix
@@ -2,8 +2,8 @@
 {
   imports = [
     ./base
-    ./features/cli
     ./features/auth
+    ./features/cli
     ./features/devtools
     #./features/productivity
     ./features/pass
@@ -23,6 +23,10 @@
       gpg.masterKey = "0xAEF4A543011E8AC1";
       gpg.signKey   = "0xA6307A244F3BD76D";
     };
+  };
+
+  xdg = {
+    enable = true;
   };
 
 }
