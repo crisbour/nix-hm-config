5f30ee4544b3c7136f44af8a35bc7ff4cf9ab8fc
Cristi Bourceanu <bourceanu.cristi@gmail.com>
2025-04-30 10:30:06 +0100

    Added lab(w6725) ssh-key to sops encryption

---
+++ hosts/lab/sops.nix
@@ -0,0 +1,20 @@
+{ inputs, ... }:
+{
+  imports = [
+    # Configuration via home.nix
+    inputs.sops-nix.nixosModules.sops
+  ];
+
+  sops = {
+    defaultSopsFile = ../../secrets/secrets.yaml;
+    # Choose what decryption method to use
+    #gnupg.home = "/home/cristi/.gnupg";
+    #gnupg.sshKeyPaths = [];
+    age = {
+      #sshKeyPaths = ["/home/cristi/.ssh/id_ed25519"];
+      sshKeyPaths = ["/home/cristi/.ssh/id_ed25519"];
+      keyFile = "/var/lib/sops-nix/keys.txt";
+      generateKey = true;
+    };
+  };
+}
